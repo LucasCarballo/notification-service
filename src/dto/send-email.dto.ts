f31d32c55e48eaaa76563aab4f243bf51b5f28d4
lucas <lucas.d.carballo@gmail.com>
2023-11-17 23:43:03 -0300

    added swagger

--- src/dto/send-email.dto.ts
+++ src/dto/send-email.dto.ts
@@ -1,7 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { EmailType } from 'src/enums/emailType';
 
 export class SendEmailDto {
+  @ApiProperty({
+    enum: EmailType,
+  })
   type: EmailType;
+
+  @ApiProperty()
   to: string;
+
+  @ApiProperty()
   message: string;
 }
